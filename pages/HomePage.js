d9f371d490753cb71ce682f8726c0b1a019280d8
FedericoMartucci <federicomartucci@hotmail.com>
2024-03-13 22:48:41 -0300

    feat: add more tests.

--- pages/HomePage.js
+++ pages/HomePage.js
@@ -13,6 +13,17 @@ class HomePage {
     this.followTinchoButton = page.locator('button[mode="follow"]').nth(0);
     this.showMoreLink = page.getByText("Show more");
     this.messageButton = page.getByText("Message");
+    this.threeDotsButton = page
+      .locator('.sc-jOHGOj.jaNlsc img[alt="dot"]')
+      .first();
+    this.firstLogoutButton = page.getByText("Log out @federicoMartucci03");
+    this.switchToSpanishButton = page.getByText("Switch to Spanish");
+    this.switchToEnglishButton = page.getByText("Cambiar a Ingles");
+    this.lastLogoutButton = page.getByRole("button", { name: "Log out" });
+    this.retweetButton = page.locator('button img[alt="retweet-icon"]').first();
+    this.retweetCounter = page.locator(
+      'div:has(> button img[alt="retweet-icon"]) label'
+    ).nth(1);
     // this.uploadImage = page
     //   .locator('input[type="file"]')
     //   .setInputFiles("../data/testImage.jpeg");
@@ -42,8 +53,36 @@ class HomePage {
   async clickShowMore() {
     this.showMoreLink.click();
   }
+
   async clickMessages() {
     this.messageButton.click();
+  }
+
+  async clickThreeDots() {
+    this.threeDotsButton.click();
+  }
+
+  async clickFirstLogoutButton() {
+    this.firstLogoutButton.click();
+  }
+
+  async switchToSpanish() {
+    this.switchToSpanishButton.click();
+  }
+  async switchToEnglish() {
+    this.switchToEnglishButton.click();
+  }
+
+  async clickLastLogoutButton() {
+    this.lastLogoutButton.click();
+  }
+
+  async retweet() {
+    this.retweetButton.click();
+  }
+
+  async cancelRetweet() {
+    this.retweetButton.click();
   }
 }
 
--- tests/HomeTests.spec.js
+++ tests/HomeTests.spec.js
@@ -7,13 +7,17 @@ test.beforeEach(async ({ page }) => {
   await page.goto("https://frontend-training-taupe.vercel.app/login");
 });
 
-
 test("User can tweet a message successfully", async ({ page }) => {
   const loginPage = new LoginPage(page);
   const homePage = new HomePage(page);
+
   await loginPage.login("federicoMartucci03", "Password123.");
+  await page.waitForTimeout(2000);
+
   await homePage.tweetMessage("Example tweet");
   await homePage.tweet();
+  await page.waitForTimeout(2000);
+
   await expect(homePage.tweetText).toBeVisible();
 });
 
@@ -22,6 +26,7 @@ test("User can follow other users from home page", async ({ page }) => {
   const loginPage = new LoginPage(page);
   const homePage = new HomePage(page);
   const profilePage = new ProfilePage(page);
+
   await loginPage.login("federicoMartucci03", "Password123.");
   await page.waitForTimeout(3000);
   await homePage.followTincho();
@@ -30,11 +35,60 @@ test("User can follow other users from home page", async ({ page }) => {
   await page.waitForTimeout(2000);
   await page.goto(
     "https://frontend-training-taupe.vercel.app/profile?user=6bef2a19-35b3-496c-b2ed-065ba1c6162b"
-    );
-    await page.waitForTimeout(2000);
-    await profilePage.unfollowTincho();
-    await page.waitForTimeout(1000);
-  });
+  );
+  await page.waitForTimeout(2000);
+  await profilePage.unfollowTincho();
+  await page.waitForTimeout(1000);
+});
+
+test("User can be able to log out", async ({ page }) => {
+  const loginPage = new LoginPage(page);
+  const homePage = new HomePage(page);
+
+  await loginPage.login("federicoMartucci03", "Password123.");
+  await page.waitForTimeout(3000);
+
+  await homePage.clickThreeDots();
+  await homePage.clickFirstLogoutButton();
+  await homePage.clickLastLogoutButton();
+  await page.waitForTimeout(3000);
+
+  expect(page.url()).toBe("https://frontend-training-taupe.vercel.app/login");
+});
+
+test("User can be able to change language", async ({ page }) => {
+  const loginPage = new LoginPage(page);
+  const homePage = new HomePage(page);
+
+  await loginPage.login("federicoMartucci03", "Password123.");
+  await page.waitForTimeout(3000);
+  
+  await homePage.clickThreeDots();
+  await page.waitForTimeout(1000);
+  await homePage.switchToSpanish();
+  await page.waitForTimeout(1000);
+  
+  expect(await homePage.homeText.isVisible()).toBe(false);
+
+  await homePage.switchToEnglish();
+});
+test("User can retweet", async ({ page }) => {
+  const loginPage = new LoginPage(page);
+  const homePage = new HomePage(page);
+
+  await loginPage.login("federicoMartucci03", "Password123.");
+  await page.waitForTimeout(3000);
+  
+  await homePage.retweet();
+  await page.waitForTimeout(3000);
+
+  expect(await homePage.retweetCounter.innerText()).toBe('1');
+  
+  await homePage.cancelRetweet();
+  await page.waitForTimeout(3000);
+  
+  expect(await homePage.retweetCounter.innerText()).toBe('0');
+});
 // test("User can tweet a picture successfully", async ({ page }) => {
 //   const loginPage = new LoginPage(page);
 //   const homePage = new HomePage(page);
